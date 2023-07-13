166fd41dcd0ae92153ec0ad6b83044bdb86e8354
reconman <reconman@users.noreply.github.com>
2023-07-14 01:22:12 +0200

    Renamed questions channel

--- src/slashCommands/questions.ts
+++ src/slashCommands/questions.ts
@@ -4,8 +4,8 @@ import { SlashCommand } from "../types";
 const QuestionsCommand : SlashCommand = {
     cooldown: 300,
 	command: new SlashCommandBuilder()
-		.setName('questions')
-		.setDescription('Refer users to #questions')
+		.setName('questions-and-problems')
+		.setDescription('Refer users to #questions-and-problems')
         .addUserOption((option) => option
             .setName('user')
             .setDescription('The user to inform')
@@ -13,11 +13,11 @@ const QuestionsCommand : SlashCommand = {
         ),
 	execute: interaction => {
         const target = interaction.options.getUser('user')
-        const questionsChannel = interaction.guild?.channels.cache.find(channel => channel.name === "questions")
+        const questionsChannel = interaction.guild?.channels.cache.find(channel => channel.name === "questions-and-problems")
         const commonProblemsChannel = interaction.guild?.channels.cache.find(channel => channel.name === "common-problems")
 		interaction.reply(
 `
-${target} please check ${commonProblemsChannel} and recently asked questions in ${questionsChannel}.
+${target} please check ${commonProblemsChannel} and recently asked questions and reported problems in ${questionsChannel}.
 
 If you don't find your problem under them, create a post in ${questionsChannel} with a detailed explanation and screenshots. Alternatively, if you have Android 12 or below you should record a video: More options -> Advanced -> Debug mode + Record Screen
 `
