3085a32f13715f4d9aa82a62faccd72dd5bada6c
reconman <reconman@users.noreply.github.com>
2023-08-01 17:32:02 +0200

    Use more robust user mention

--- src/events/threadCreate.ts
+++ src/events/threadCreate.ts
@@ -1,4 +1,4 @@
-import { ChannelType, ThreadChannel } from "discord.js";
+import { ChannelType, ThreadChannel, userMention } from "discord.js";
 import { BotEvent } from "../types";
 import { ForumChannel } from "discord.js";
 
@@ -10,9 +10,8 @@ const event : BotEvent = {
             const forumChannel = channel as ForumChannel
             const problemTag = forumChannel.availableTags.filter(tag => tag.name == "Problem")[0]
             if (thread.appliedTags.includes(problemTag.id)) {
-                const startingMessage = await thread.fetchStarterMessage()
                 thread.send(`
-${startingMessage?.author} we'll need some information before we can help you:
+${userMention(thread.ownerId!!)} we'll need some information before we can help you:
 
 1. Which FGA Version are you using? (Can be found in the app's starting screen at the top)
 2. Are you using the Playstore or Github app version?
