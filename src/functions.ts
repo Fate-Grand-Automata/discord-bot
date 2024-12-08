04e88100ecd8a9eccf8856d78e500a23eb12dccc
reconman <reconman@users.noreply.github.com>
2024-12-08 18:20:34 +0100

    Correct indentation of bug questions

--- src/functions.ts
+++ src/functions.ts
@@ -47,10 +47,10 @@ ${user} we'll need some information before we can help you:
 4. What's your Android version?
 5. Did you change any Fine-Tune settings? (Similarity, wait times, ...) If yes, what did you change?
 6. If the problems happens in FGO, record a video:
- - On Android 12, go to More Options -> Advanced and enable Debug mode and Record Screen.
+  - On Android 12 or below, go to More Options -> Advanced and enable Debug mode and Record Screen.
     
     Your FGA directory will contain a record.mp4 file after you've pressed the Stop button. You probably have to upload it to Youtube or streamable.com to share it here.
- - On Android 13 or above, follow this guide: https://youtu.be/iIjZPjDcZeM
+  - On Android 13 or above, follow this guide: https://youtu.be/iIjZPjDcZeM
     
     If you don't have a PC, then at least post screenshots of the problematic areas.
 `
