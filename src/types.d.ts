0584b75fdb070475ff6af627c97a280708e77e8a
reconman <reconman@users.noreply.github.com>
2024-12-09 22:48:49 +0100

    Fix build errors

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,9 @@
-import { SlashCommandBuilder, CommandInteraction, Collection, PermissionResolvable, Message, AutocompleteInteraction } from "discord.js"
+import { SlashCommandBuilder, ChatInputCommandInteraction, Collection, PermissionResolvable, Message, AutocompleteInteraction } from "discord.js"
 import mongoose from "mongoose"
 
 export interface SlashCommand {
     command: SlashCommandBuilder | any,
-    execute: (interaction : CommandInteraction) => void,
+    execute: (interaction : ChatInputCommandInteraction) => void,
     autocomplete?: (interaction: AutocompleteInteraction) => void,
     cooldown?: number // in seconds
 }
